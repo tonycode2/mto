086528c1831f6c7860368a0d8021caeb84131232
tonycode2 <velurn123@gmail.com>
2025-08-15 18:58:03 -0600

    initial commit

---
+++ mto/src/main/java/com/anthony/project/mto/mto/MtoApplication.java
@@ -0,0 +1,13 @@
+package com.anthony.project.mto.mto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtoApplication.class, args);
+	}
+
+}
---
+++ mto/src/test/java/com/anthony/project/mto/mto/MtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anthony.project.mto.mto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
